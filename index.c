8afcb63139be910c53345425963a3875e544be33
Danimell061 <dani9ct@gmail.com>
2024-09-26 15:26:47 -0300

    Criando projeto

---
+++ index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    printf("Hello Word!");
+
+    printf("\n\n");
+    return 0;
+}
