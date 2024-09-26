703c82b039b435b09605623a81f572bcc7fea643
Danimell061 <dani9ct@gmail.com>
2024-09-26 15:57:22 -0300

    Atualizando

--- index.c
+++ index.c
@@ -2,10 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char const *argv[])
+void Troca(int array[], int n)
+{
+    int i, j, temp;
+
+    // Ira ler termo por termo
+    for (i = 0; i < n; i++)
+    {
+        // Ira comparar termo por termo
+        for (j = 0; j < n - 1; j++)
+        {
+            // Troca os elementos
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main(void)
 {
     system("cls");
-    printf("Hello Word!");
+
+    int array[10] = { 1 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 20, 12};
+
+    Troca(array, 10);
+
+	for (int i = 0; i < 10; i++){
+    printf("%i ", array[i]);
+    }
 
     printf("\n\n");
     return 0;
