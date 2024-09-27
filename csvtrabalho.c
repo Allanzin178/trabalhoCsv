fcc29e84636a23b8ac6192b4b7b127b2a1f3b3e0
Allanzin178 <allan.miron@hotmail.com>
2024-09-27 18:13:37 -0300

    mermao

--- csvtrabalho.c
+++ csvtrabalho.c
@@ -7,8 +7,8 @@ typedef struct {
     int id;
     char numero[255];
     char data_ajuizamento[255];
-    int *id_classe;
-    int *id_assunto;
+    int id_classe[8];
+    int id_assunto[8];
     int ano_eleicao;
 } Processo;
 
@@ -31,6 +31,7 @@ int main(){
 
     // ------------------------------------------------------------------
 
+
     lerCsv(fp, p);
 
     free(p);
@@ -39,14 +40,12 @@ int main(){
 
 Processo *criarProcesso(int numLinhas, int maxClassesAssuntos){
     Processo *p = (Processo*) malloc(numLinhas * sizeof(Processo));
-    p->id_classe = (int*) malloc(maxClassesAssuntos * sizeof(int));
-    p->id_assunto = (int*) malloc(maxClassesAssuntos * sizeof(int));
-
-    if(p == NULL || p->id_classe == NULL || p->id_assunto == NULL){
+    if(p == NULL){
         printf("Erro na alocação de memória (malloc)");
-        free(p);
         exit(1);
     }
+
+    
     memset(p->numero, 0, sizeof(p->numero));
     memset(p->data_ajuizamento, 0, sizeof(p->data_ajuizamento));
     p->id = 0;
@@ -63,61 +62,89 @@ int contLinhas(FILE *fp){
 }
 
 void lerCsv(FILE *fp, Processo *p){
-    int i, j, cont, virg = 0;
+    int i, j, k, cont, virg[2] = {0, 0}, fChave = 0;
     char linha[255];
     char *rem;
-    
-    // for(i = 30; i < sizeof(linha); i++){
-    //     if(linha[i] == '{'){
-    //         printf("teste");
-    //         for(j = i; linha[j] != '}'; j++){
-    //             if (linha[j] == ','){
-    //                 virg++;
-    //             }
-    //         }
-    //     }
-    // }
-    
 
     for(i = 0; fgets(linha, sizeof(linha), fp); i++){
         cont = 0;
         if(i == 0){
             continue;
         }
-        printf("%s", linha);
-        for(i = 30; i < sizeof(linha); i++){
-            if(linha[i] == '{'){
-                for(j = i; linha[j] != '}'; j++){
-                    if (linha[j] == ','){
-                        virg++;
+        // NAO TOCA NO MEU CODIGO :<
+            for(i = 30; i < sizeof(linha); i++){
+                if(linha[i] == '{'){  
+                    for(j = i; linha[j] != '}' && fChave == 0; j++){
+                        if (linha[j] == ','){
+                            virg[0] = virg[0] + 1;
+                        }
+                        if (linha[j + 1] == '}'){
+                            fChave++;
+                        }
                     }
+                    if(linha[i-3] == '}'){
+                        for(j = i; linha[j] != '}' && fChave > 0; j++){
+                            if (linha[j] == ','){
+                                virg[1] = virg[1] + 1;
+                            }
+                            if (linha[j + 1] == '}'){
+                                fChave++;
+                            }
+                        }
+                    }
+                    
                 }
             }
+
+        // NAO TOCA NO MEU CODIGO :<
+
+        rem = strtok(linha, ",\"{}");
+        // Armazenando
+        while(rem != NULL && cont < 6){
+            // Inicio do switch
+            switch(cont){
+                case 0:
+                    p[i - 1].id = atoi(rem);
+                    printf("%d ", p[i - 1].id);
+                    rem = strtok(NULL, ",\"{}");
+                    break;
+                
+                case 1:
+                    strcpy(p[i-1].numero, rem);
+                    printf("%s ", p[i - 1].numero);
+                    rem = strtok(NULL, ",\"{}");
+                    break;
+                case 2:
+                    strcpy(p[i-1].data_ajuizamento, rem);
+                    printf("%s ", p[i - 1].data_ajuizamento);
+                    rem = strtok(NULL, ",\"{}");
+                    break;
+                case 3:
+                    printf(" | ");
+                    printf("virg: %d ",virg[0]);
+                    for(j = 0; j <= virg[0]; j++){
+                        p[i-1].id_classe[j] = atoi(rem);
+                        printf("%d ", p[i - 1].id_classe[j]);
+                        rem = strtok(NULL, ",\"{}");
+                    }
+                    printf(" | ");
+                    break;
+                case 4:
+                    printf("virg1: %d ",virg[1]);
+                    for(j = 0; j <= virg[1]; j++){
+                        p[i-1].id_assunto[j] = atoi(rem);
+                        printf("%d ", p[i - 1].id_assunto[j]);
+                        rem = strtok(NULL, ",\"{}");
+                    }
+                    printf("\n");
+                    break;
+            }
+            
+            cont++;
+            
         }
-        printf("virg%d\n", virg);
-        virg = 0;
-        // rem = strtok(linha, ",");
-        // // Armazenando
-        // while(rem != NULL && cont < 6){
-        //     // Inicio do switch
-        //     switch(cont){
-        //         case 0:
-        //             p[i - 1].id = atoi(rem);
-        //             printf("%d ", p[i - 1].id);
-        //             break;
-                
-        //         case 1:
-        //             strcpy(p[i-1].numero, rem);
-        //             printf("%s ", p[i - 1].numero);
-        //             break;
-        //         case 2:
-        //             strcpy(p[i-1].data_ajuizamento, rem);
-        //             printf("%s\n", p[i - 1].data_ajuizamento);
-        //             break;
-        //     }
-        //     rem = strtok(NULL, ",");
-        //     cont++;
-                
-        // }
+        virg[0] = 0;
+        virg[1] = 0;
+        fChave = 0;
     }
 }
